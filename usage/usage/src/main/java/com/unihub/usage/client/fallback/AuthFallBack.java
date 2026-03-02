4669d9a134b14a7792f3da2593c809955542db26
abedelrahaman <lazo.blogz@gmail.com>
2026-03-02 05:03:30 +0200

    SCRUM:42 fix(dashboard-analysis) removed unrequired parameter

--- usage/usage/src/main/java/com/unihub/usage/client/fallback/AuthFallBack.java
+++ usage/usage/src/main/java/com/unihub/usage/client/fallback/AuthFallBack.java
@@ -16,6 +16,6 @@ public class AuthFallBack implements AuthFeignClient {
     @Override
     public ResponseEntity<DashBoardAggregatesDto> getUserAnalysis(UUID tid, String apiKey) {
         log.error("Feign client fallback triggered: Auth service is unavailable.");
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(emptyDto);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
